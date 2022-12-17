69880c1f7388ee1a14e1a1a9a4a838313bfc74b2
TimofeyBelikov <55021882+TimofeyBelikov@users.noreply.github.com>
2022-12-18 00:49:09 +0300

    Update settings.py

--- Practice9_Second/settings.py
+++ Practice9_Second/settings.py
@@ -13,7 +13,7 @@ import os
 import django_opentracing
 from pathlib import Path
 import opentracing
-from jaeger_client import 
+from jaeger_client import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
